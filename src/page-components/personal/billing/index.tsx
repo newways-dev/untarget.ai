d050124a021ae1538fcf549af69a88e8e2e8d2f3
newways-dev <reactwebdew@gmail.com>
2023-03-13 12:10:03 +0300

    billing

--- src/page-components/personal/billing/index.tsx
+++ src/page-components/personal/billing/index.tsx
@@ -6,22 +6,40 @@ export const Billing = () => {
       <div className={styles.balance}>
         <div className={styles.top}>
           <p>Сумма *</p>
-          <div>
+          <div className={styles.badges}>
             <input type='text' placeholder='0 ₽' />
             <div>500 ₽</div>
             <div>2500 ₽</div>
             <div>5000 ₽</div>
           </div>
-          <p>Отправить чек на адрес *</p>
-          <input type='email' placeholder='Email' />
-          <div className={styles.terms}></div>
-          <div>
-            <button>Пополнить баланс</button>
+        </div>
+        <p>Отправить чек на адрес *</p>
+        <input type='email' placeholder='Email' />
+        <div className={styles.terms}>
+          <div className={styles.termsWrapper}>
+            <label className={styles.checkboxContainer}>
+              <input type='checkbox' />
+              <span className={styles.checkmark} />
+            </label>
             <p>
-              Поделитесь, пожалуйста, своим опытом использования сервиса оставив
-              <span>отзыв</span>
+              Пополнять ежемесячно, принимаю{' '}
+              <span>Офферта регулярных платежей</span>
             </p>
           </div>
+          <div className={styles.termsWrapper}>
+            <label className={styles.checkboxContainer}>
+              <input type='checkbox' />
+              <span className={styles.checkmark} />
+            </label>
+            <p>Сохранить платежный идентификатор для оплаты в один клик</p>
+          </div>
+        </div>
+        <div className={styles.button}>
+          <button>Пополнить баланс</button>
+          <p>
+            Поделитесь, пожалуйста, своим опытом использования сервиса оставив{' '}
+            <span>отзыв</span>
+          </p>
         </div>
       </div>
     </div>
